cbb8677b86ee35e087c177ca78a1f063b2fb9db2
67Samuel <60543441+67Samuel@users.noreply.github.com>
2020-08-05 18:32:05 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,7 @@ def args_parser_train():
 	returns argument parser object used while training a model
 	"""
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--architecture', required=True, type=str, help='neural network architecture [vgg19, resnet50]') 
+	parser.add_argument('--architecture', required=True, type=str, help='neural network architecture [vgg19, resnet50, alexnet]') 
 	parser.add_argument('--dataset',type=str, required=True, help='dataset [cifar10, cifar100, svhn, fashionmnist]')
 	parser.add_argument('--batch-size', type=int, default=512, help='input batch size for training (default: 512)')
 	parser.add_argument('--optimizer', type=str, default='sgd', help='optimizer [sgd, adam]')
@@ -30,7 +30,7 @@ def args_parser_iterprune():
 	returns argument parser object used for iterative pruning
 	"""
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--architecture', type=str, metavar='arch',required=True, help='neural network architecture [vgg19, resnet50]') 
+	parser.add_argument('--architecture', type=str, metavar='arch',required=True, help='neural network architecture [vgg19, resnet50, alexnet]') 
 	parser.add_argument('--target-dataset',type=str, required=True, help='dataset [cifar10, cifar100, svhn, fashionmnist]')
 	parser.add_argument('--batch-size', type=int, default=512,help='input batch size for training (default: 512)')
 	parser.add_argument('--source-dataset',type=str, required=True, help='dataset [cifar10, cifar100, svhn, fashionmnist]')
@@ -48,7 +48,7 @@ def args_parser_test():
 	returns argument parser object used while testing a model
 	"""
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--architecture', type=str, metavar='arch', required=True, help='neural network architecture [vgg19, resnet50]') 
+	parser.add_argument('--architecture', type=str, metavar='arch', required=True, help='neural network architecture [vgg19, resnet50, alexnet]') 
 	parser.add_argument('--dataset',type=str, required=True, help='dataset [cifar10, cifar100, svhn, fashionmnist]')
 	parser.add_argument('--batch-size', type=int, default=512, help='input batch size for training (default: 512)')
 	parser.add_argument('--model_path',type=str, required=True, help='path to the model for finding test accuracy')
@@ -641,5 +641,48 @@ def load_model(architecture, num_classes):
 		                   pretrained, progress, **kwargs)
 
 		return resnet50(pretrained=False)
+	
+	elif architecture == "alexnet":
+		class AlexNet(nn.Module):
+		    def __init__(self, in_channels=3, init_padding=(2, 2), classes=200):
+			super(AlexNet, self).__init__()
+			self.features = nn.Sequential(
+			    nn.Conv2d(in_channels, 64, kernel_size=11, stride=4, padding=init_padding),
+			    nn.ReLU(inplace=True),
+			    nn.MaxPool2d(kernel_size=3, stride=2),
+			    nn.Conv2d(64, 192, kernel_size=5, padding=2),
+			    nn.ReLU(inplace=True),
+			    nn.MaxPool2d(kernel_size=3, stride=2),
+			    nn.Conv2d(192, 384, kernel_size=3, padding=1),
+			    nn.ReLU(inplace=True),
+			    nn.Conv2d(384, 256, kernel_size=3, padding=1),
+			    nn.ReLU(inplace=True),
+			    nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			    nn.ReLU(inplace=True),
+			    nn.MaxPool2d(kernel_size=3, stride=2),
+			)
+			self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+			self.classifier = nn.Sequential(
+			    nn.Dropout(),
+			    nn.Linear(256 * 6 * 6, 4096),
+			    nn.ReLU(inplace=True),
+			    nn.Dropout(),
+			    nn.Linear(4096, 4096),
+			    nn.ReLU(inplace=True),
+			    nn.Linear(4096, classes),
+			)
+
+		    def forward(self, x):
+			x = self.features(x)
+			x = self.avgpool(x)
+			x = x.view(x.size(0), 256 * 6 * 6)
+			x = self.classifier(x)
+			return x
+
+		def createAlexNet(in_channels=3, init_padding=(2, 2), classes=100):
+		    return AlexNet(in_channels, init_padding, classes)
+	
+		alexnet = createAlexNet(classes=num_classes)
+		return alexnet
 	else:
 		raise ValueError(architecture + " architecture not supported.")
