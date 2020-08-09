e157a514917ee8b4be0cca66b92cdfe58debecea
67Samuel <60543441+67Samuel@users.noreply.github.com>
2020-08-09 18:06:37 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -88,7 +88,7 @@ def train(model, args, dataloader, device):
 		if (early_stopper.early_stop == True):
 			if all(num > epoch for num in lr_anneal_epochs):
 				print(f"epoch {epoch} > {lr_anneal_epochs[-1]} and early stopping is true. Stopping training...")
-                		break
+				break
 			else:
 				optimizer.param_groups[0]['lr'] /= 10
 
